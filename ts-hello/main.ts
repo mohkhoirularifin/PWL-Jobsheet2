c155a8a01cc91bce460152f2e7507886377141e1
Moh. Khoirul Arifin <karifin998@gmail.com>
2019-02-13 19:53:49 +0700

    Update main.ts

--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -47,15 +47,31 @@
 //     y:3,
 // })
 
-class Pointku{
+// class Pointku{
+//     a:number;
+//     b:number;
+
+//     draw(){
+//         //...
+//     }
+
+//     getDistance(another:Pointku){
+//         //....
+//     }
+// }
+
+class Pointku1{
     a:number;
     b:number;
 
     draw(){
-        //...
+        console.log('X' + this.a + ',Y' + this.b);
     }
 
-    getDistance(another:Pointku){
+    getDistance(another:Pointku1){
         //....
     }
 }
+
+let coins:Pointku1
+coins.draw();
