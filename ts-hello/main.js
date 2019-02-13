4051b58fbf4405a593c82ea0471078d0c368b52e
Moh. Khoirul Arifin <karifin998@gmail.com>
2019-02-13 19:35:54 +0700

    Create main.js

---
+++ ts-hello/main.js
@@ -0,0 +1,26 @@
+// tslint:disable-next-line:no-shadowed-variable
+// function log(message){
+//     console.log(message);
+// }
+// let message = 'Hello WOrld';
+// log(message);
+// function doSomething(){
+//     for (let i = 0; i < 5; i++) {
+//         console.log(i);
+//     }
+//     console.log('Finally: ' + i);
+// }
+// doSomething();
+// let count = 5;
+// count = 'a';
+// let pesan;
+// pesan='abc';
+// let percobaan=(<string>pesan).endsWith('c');
+// let alternative=(pesan as string).endsWith('c');
+var log = function pesan() {
+    console.log(pesan);
+};
+var doLog = function (pesan) {
+    console.log(pesan);
+};
+var doLog1 = function (pesan) { return console.log(pesan); };
